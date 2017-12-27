480eb52d06e96e11e7956c46a04d42be7e3413fb
ramaki5 <rk.mundru@gmail.com>
2017-12-27 16:00:03 +0530

    First commit

---
+++ TestDemo/src/main/java/com/pluralsight/repository/HelloService.java
@@ -0,0 +1,10 @@
+package com.pluralsight.repository;
+
+public class HelloService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestDemo/src/main/java/com/pluralsight/service/HelloRepository.java
@@ -0,0 +1,10 @@
+package com.pluralsight.service;
+
+public class HelloRepository {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
